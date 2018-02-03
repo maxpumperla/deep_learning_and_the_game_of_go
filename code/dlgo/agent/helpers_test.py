a18dc1596eed12281b6398e2b1c4bd46d9ebf7eb
Max Pumperla <max.pumperla@googlemail.com>
2018-02-03 11:42:25 +0100

    update code for chapter 3

---
+++ code/dlgo/agent/helpers_test.py
@@ -0,0 +1,39 @@
+import unittest
+
+from .helpers import is_point_an_eye
+from ..goboard import Board
+from ..gotypes import Player, Point
+
+
+class EyeTest(unittest.TestCase):
+    def test_corner(self):
+        board = Board(19, 19)
+        board.place_stone(Player.black, Point(1, 2))
+        board.place_stone(Player.black, Point(2, 2))
+        board.place_stone(Player.black, Point(2, 1))
+        self.assertTrue(is_point_an_eye(board, Point(1, 1), Player.black))
+        self.assertFalse(is_point_an_eye(board, Point(1, 1), Player.white))
+
+    def test_corner_false_eye(self):
+        board = Board(19, 19)
+        board.place_stone(Player.black, Point(1, 2))
+        board.place_stone(Player.black, Point(2, 1))
+        self.assertFalse(is_point_an_eye(board, Point(1, 1), Player.black))
+        board.place_stone(Player.white, Point(2, 2))
+        self.assertFalse(is_point_an_eye(board, Point(1, 1), Player.black))
+
+    def test_middle(self):
+        board = Board(19, 19)
+        board.place_stone(Player.black, Point(2, 2))
+        board.place_stone(Player.black, Point(3, 2))
+        board.place_stone(Player.black, Point(4, 2))
+        board.place_stone(Player.black, Point(4, 3))
+        board.place_stone(Player.white, Point(4, 4))
+        board.place_stone(Player.black, Point(3, 4))
+        board.place_stone(Player.black, Point(2, 4))
+        board.place_stone(Player.black, Point(2, 3))
+        self.assertTrue(is_point_an_eye(board, Point(3, 3), Player.black))
+
+
+if __name__ == '__main__':
+    unittest.main()
