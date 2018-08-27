977672147aa7c284ae692c875754fb59ea5f19e4
Max Pumperla <max.pumperla@googlemail.com>
2018-08-27 11:40:42 +0200

    Update setup.py

--- code/setup.py
+++ code/setup.py
@@ -5,7 +5,7 @@ setup(name='dlgo',
       version='0.1',
       description='Deep Learning and the Game of Go',
       url='http://github.com/maxpumperla/deep_learning_and_the_game_of_go',
-      install_requires=['tensorflow', 'keras', 'gomill', 'Flask>=0.10.1', 'Flask-Cors', 'future', 'h5py', 'six'],
+      install_requires=['numpy<=1.14.5', 'tensorflow', 'keras', 'gomill', 'Flask>=0.10.1', 'Flask-Cors', 'future', 'h5py', 'six'],
       license='MIT',
       packages=find_packages(),
       zip_safe=False)
